05709c330509f878e333610c1c82a53a6d31deb2
aiswarya <aiswaryapnair4@gmail.com>
2023-03-21 22:44:07 -0600

    completed

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { GalleryService } from '../gallery.service';
-
+import { collection } from '@firebase/firestore';
+import { Firestore } from '@angular/fire/firestore';
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
@@ -9,7 +10,8 @@ import { GalleryService } from '../gallery.service';
 })
 export class PaymentComponent {
   dig:any
-  constructor(private d:GalleryService,private router:Router){}
+  gallery: any;
+  constructor(private d:GalleryService,private router:Router,private fire:Firestore){}
   ngOnInit(){
     let idd:any=localStorage.getItem('id')
     let arrd=this.d.getdgDB();
@@ -19,5 +21,60 @@ export class PaymentComponent {
   gotohome(){
     this.router.navigate(['/home'])
   }
+  
+  firstname:string='';
+  lastname:string='';
+  username:string='';
+  email:string='';
+  address:string='';
+  address2:string='';
+  country:string='';
+  state:string='';
+  credit:string='';
+  debit:string='';
+  paypal:string='';
+  name:string='';
+  number:string='';
+  date:string='';
+  cvv:string='';
+submit()
+{
+  const data={
+  firstname:this.firstname,
+  lastname:this.lastname,
+  username:this.username,
+  email:this.email,
+  address:this.address,
+  address2:this.address2,
+  country:this.country,
+  state:this.state,
+
+  name:this.name,
+  number: this.number,
+  date:this.date,
+  cvv:this.cvv,
+  }
+  console.log(data);
+  const docRef=collection(this.fire,"payment")
+  this.d.insertData(docRef,data)
+  this.firstname=''
+  this.lastname=''
+  this.username=''
+  this.email=''
+  this.address=''
+  this.address2=''
+  this.country=''
+  this.state=''
+  this.credit=''
+  this.debit=''
+  this.paypal=''
+  this.name=''
+  this.date=''
+  this.cvv=''
+  alert( 'bookingsuccess')
+}
+  
 
 }
+
+
